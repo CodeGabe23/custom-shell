2135ba5abba211200b1d027a89477173cf362833
CodeGabe23 <gabrielcristiandan@hotmail.com>
2026-06-03 18:05:43 +0300

    Finished create file function

---
+++ src/create_file.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <linux/kernel.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+std::string create_file(std::string filename)
+{
+	// check if the file already exists
+	if (access(filename.c_str(), F_OK) != -1) return "File already created.";
+	
+	// create the file
+	open(filename.c_str(), O_CREAT);
+	return "File created succesfully.";
+}
+
+int main()
+{
+	create_file("test.txt");
+}
