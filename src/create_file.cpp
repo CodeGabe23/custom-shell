0240cdfc668ba457ab9c9f426f639527ee04c32b
CodeGabe23 <gabrielcristiandan@hotmail.com>
2026-06-03 21:35:51 +0300

    User interface complete

--- src/create_file.cpp
+++ src/create_file.cpp
@@ -1,7 +1,14 @@
 #include "create_file.hpp"
 
-std::string create_file(std::string filename)
+std::string create_file()
 {
+	system("cls");
+
+	std::string filename {};
+
+	std::cout << "Enter the filename: ";
+	std::getline(std::cin, filename);
+
 	// check if the file already exists
 	if (access(filename.c_str(), F_OK) != -1) return "File already created.";
 	
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,48 @@
+#include <fstream>
 #include "create_file.hpp"
 
 int main()
 {
-	std::string statusCreateFile = create_file("bomboclat.txt");
+	system("clear");
+
+	// Get User Data
+	system("mkdir .userdata");
+	system("clear");
+
+	system("whoami >> .userdata/username.txt");
+	system("hostname >> .userdata/hostname.txt");
+	system("pwd >> .userdata/currentDir.txt");
+
+	// 'f' prefix is for file
+	std::ifstream fuserName(".userdata/username.txt");
+	std::ifstream fhostname(".userdata/hostname.txt");
+	std::ifstream fcurrentDir(".userdata/currentDir.txt");
+	
+	std::string userName, hostName, currentDir;
+
+	getline(fuserName, userName);
+	fuserName.close();
+
+	getline(fhostname, hostName);
+	fhostname.close();
+
+	getline(fcurrentDir, currentDir);
+	fcurrentDir.close();
+	
+	std::cout << "Welcome, " << userName << "\n\n";
+
+	//TODO: Parse input to see what the user wants to do	
+	
+	std::string userInput{};
+
+	while (true)
+	{
+		std::cout << "\033[32m" << userName << "@" << hostName << "\033[0m:";
+		std::cout << "\033[34m" << currentDir << "\033[0m$ ";
+
+		std::getline(std::cin, userInput);
+	}
+
 
 	return 0;
 }
